2f9b02b5fe20a374790b4b64bb81ace8c8d87a99
John Hession <johhess@cdw.com>
2022-02-03 09:01:18 -0800

    fixing poor directory structure

--- docfx.go
+++ docfx.go
@@ -46,13 +46,12 @@ func GenerateFxStruct(g GitlabData)error{
 		return err
 	}
 
-	err = os.Chdir(fmt.Sprintf("%v", "../DocFxData"))
-	if err != nil {
-		return fmt.Errorf(err.Error())
-
-	}
-
 	for _, v := range g.ProjectIds {
+		err = os.Chdir(fmt.Sprintf("%v", "../DocFxData"))
+		if err != nil {
+			return fmt.Errorf(err.Error())
+
+		}
 
 		data, err := GetGitLabProjectData(v, g.Token)
 		if err != nil {
